322e163401cd31581959341ef5db05e94a1728d2
kid-116 <mehultodi116@gmail.com>
2021-01-15 13:10:38 +0530

    implemented cgpa validation

--- session_2/student_registry/app/models/student.rb
+++ session_2/student_registry/app/models/student.rb
@@ -1,2 +1,12 @@
+class CGPAValidator < ActiveModel::Validator
+    def validate(record)
+        unless record.cgpa > 0 and record.cgpa < 10
+            raise ArgumentError, "Invalid CGPA"
+        end
+    end
+end
+
 class Student < ApplicationRecord
+    include ActiveModel::Validations
+    validates_with CGPAValidator
 end
