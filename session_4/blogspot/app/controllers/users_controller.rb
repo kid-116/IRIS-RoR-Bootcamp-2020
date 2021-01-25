6b402f907174e2584be90ca71659dfc76a496f25
kid-116 <mehultodi116@gmail.com>
2021-01-25 10:22:09 +0530

    Implemented session_4 tasks

---
+++ session_4/blogspot/app/controllers/users_controller.rb
@@ -0,0 +1,91 @@
+class UsersController < ApplicationController
+  load_and_authorize_resource
+  before_action :set_user, only: %w[ show edit update destroy ]
+
+  # GET /users
+  # GET /users.json
+  def index
+    @users = User.all
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(user_params)
+    if @user.admin
+      @user.private_articles_remaining = -1
+    else
+      @user.private_articles_remaining = 5
+    end
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to login_path, notice: "User was successfully created." }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: "User was successfully updated." }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      begin
+        User.find(session[:user_id])
+      rescue
+        session[:user_id] = nil
+      end
+      if current_user
+        if current_user.admin
+          format.html { redirect_to users_path, notice: "Account deleted." }
+        else
+          format.html { redirect_to articles_path, notice: "Account was successfully deleted." }
+        end
+      end
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
+    end
+end
---
+++ session_4/blogspot/app/models/ability.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    # All users
+    can :index, Article
+    can :index, User
+    can :new, User
+    can :create, User
+    can :show, User
+    
+    # Can read public articles
+    can :show, Article, public: true
+
+    # Additional permissions for logged in users
+    if user.present?
+      # Can read private articles
+      if user.private_articles_remaining != 0
+        can :show, Article, public: false
+      end
+      
+      # Can create articles
+      can :new, Article
+      can :create, Article
+
+      # Can edit their own articles
+      can :edit, Article, user_id: user.id
+      can :update, Article, user_id: user.id
+
+      # Can destroy their own articles
+      can :destroy, Article, user_id: user.id
+      
+      can :edit, User, email: user.email
+      can :update, User, email: user.email
+      
+      can :destroy, User, email: user.email
+
+      if user.admin
+        can :edit, Article
+        can :update, Article
+        can :destroy, Article
+        can :edit, User
+        can :update, User
+        can :destroy, User
+      end
+    end
+
+  end
+end
